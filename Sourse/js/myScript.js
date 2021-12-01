1520b1b687fabec675166f5b36bb2424e8971f73
Trejger <89919849+Trejger@users.noreply.github.com>
2021-12-01 17:10:03 +1000

    Add files via upload

--- Sourse/js/myScript.js
+++ Sourse/js/myScript.js
@@ -1,38 +1,33 @@
 
 
-$(document).ready(function(){
-    $('.benefits__number').spincrement({
-        thousandSeparator: "",
-        duration: 1200
-    });
+
+<script type="text/javascript">
+$(document).ready(function() {
+ $(".slider").each(function () { // обрабатываем каждый слайдер
+  var obj = $(this);
+  $(obj).append("<div class='nav'></div>");
+  $(obj).find("li").each(function () {
+   $(obj).find(".nav").append("<span rel='"+$(this).index()+"'></span>"); // добавляем блок навигации
+   $(this).addClass("slider"+$(this).index());
+  });
+  $(obj).find("span").first().addClass("on"); // делаем активным первый элемент меню
+ });
 });
-
-$(document).ready(function () {
- 
-    var show = true;
-    var countbox = ".benefits__inner";
-    $(window).on("scroll load resize", function () {
-        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-        var w_height = $(window).height(); // Высота окна браузера
-        var d_height = $(document).height(); // Высота всего документа
-        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-            $('.benefits__number').css('opacity', '1');
-            $('.benefits__number').spincrement({
-                thousandSeparator: "",
-                duration: 1200
-            });
-             
-            show = false;
-        }
-    });
- 
+function sliderJS (obj, sl) { // slider function
+ var ul = $(sl).find("ul"); // находим блок
+ var bl = $(sl).find("li.slider"+obj); // находим любой из элементов блока
+ var step = $(bl).width(); // ширина объекта
+ $(ul).animate({marginLeft: "-"+step*obj}, 500); // 500 это скорость перемотки
+}
+$(document).on("click", ".slider .nav span", function() { // slider click navigate
+ var sl = $(this).closest(".slider"); // находим, в каком блоке был клик
+ $(sl).find("span").removeClass("on"); // убираем активный элемент
+ $(this).addClass("on"); // делаем активным текущий
+ var obj = $(this).attr("rel"); // узнаем его номер
+ sliderJS(obj, sl); // слайдим
+ return false;
 });
-
-
-
+</script>
 
 
 "use strict"
@@ -146,3 +141,37 @@ if( type == 2 && style == 1 && adaptivity == 3){
 }
 
 alert("Стоимость " + finishCash + " рублей, сроки " + finishDate + " дней");
+
+
+
+
+$(document).ready(function(){
+    $('.benefits__number').spincrement({
+        thousandSeparator: "",
+        duration: 1200
+    });
+});
+
+$(document).ready(function () {
+ 
+    var show = true;
+    var countbox = ".benefits__inner";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.benefits__number').css('opacity', '1');
+            $('.benefits__number').spincrement({
+                thousandSeparator: "",
+                duration: 1200
+            });
+             
+            show = false;
+        }
+    });
+ 
+});
