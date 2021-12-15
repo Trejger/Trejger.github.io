8d5970d2b679029eeaa9867af0ba07224d5ee51e
Trejger <89919849+Trejger@users.noreply.github.com>
2021-12-15 23:31:43 +1000

    Add files via upload

--- Sourse/js/myScript.js
+++ Sourse/js/myScript.js
@@ -26,9 +26,11 @@ $(document).on("click", ".slider .nav span", function() { // slider click naviga
 });
 
 
+$(document).ready(function(){
+    new WOW().init();
+});
 
-
-let акп type = prompt("Какой сайт вам нужен? 1.Сайт-визитка 2.Корпоративный 3.Интернет-магазин");
+let type = prompt("Какой сайт вам нужен? 1.Сайт-визитка 2.Корпоративный 3.Интернет-магазин");
 let style = prompt("Дизайн? 1.Новый 2.Ретро 3.Индивидуальный");
 let adaptivity = prompt("Адаптивность? 1.Телефон 2.Компьютер 3.Два в одном");
 
