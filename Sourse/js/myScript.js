87ca41287d35cf24d310b3f1c1297d23f63e2305
Trejger <89919849+Trejger@users.noreply.github.com>
2022-01-14 20:55:24 +1000

    Add files via upload

--- Sourse/js/myScript.js
+++ Sourse/js/myScript.js
@@ -39,18 +39,6 @@ $(document).ready(function(){
 $('form').submit(function(){
 event.preventDefault()
 
-$.ajax({
-type: "POST",
-url: "php/mail.php",
-data: $(this).serialize()
-}).done(function (){
-$(this).find("input").val("");
-alert("Успешно отправлено!");
-$("form").trigger("reset");
-
-});
-return false;
-});
 
 
 });
