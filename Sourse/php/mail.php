cc536ded99e46f8b852aa178c0b362570e49f007
Trejger <89919849+Trejger@users.noreply.github.com>
2022-01-18 17:28:03 +1000

    Add files via upload

--- Sourse/php/mail.php
+++ Sourse/php/mail.php
@@ -4,22 +4,24 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
+$phone = $_POST['user_phone'];
+$radio = $_POST['radios'];
+$check = $_POST['check'];
+
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'example@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'password'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'marina.trejger@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'UPYR$ypu2rr1'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('example@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('exampleto@p33.org');     // Кому будет уходить письмо 
+$mail->setFrom('random_help@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('marina.trejger@mail.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
